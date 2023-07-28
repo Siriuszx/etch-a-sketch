7d1006e371f0ad1a7c1d7480f2a7ab1244436e63
Siriuszx <114768817+Siriuszx@users.noreply.github.com>
2023-07-28 15:26:38 +0200

    Refactor code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const grid = document.querySelector('.grid');
 
 const gridSizeField = document.querySelector('#current-grid-size')
 const gridSizeSlider = document.querySelector('#grid-dimensions-slider');
-gridSizeSlider.addEventListener('input', updateSizeField);
+gridSizeSlider.addEventListener('input', updSizeFieldHandler);
 
 let mouseToggle = false;
 let isColorRandom = false;
@@ -11,10 +11,26 @@ const clearBtn = document.querySelector('.select-value-clear');
 const submitBtn = document.querySelector('.select-value-submit');
 const rndColorBtn = document.querySelector('.select-color');
 clearBtn.addEventListener('click', resetGrid)
-submitBtn.addEventListener('click', resizeGrid);
-rndColorBtn.addEventListener('click', toggleColoring);
+submitBtn.addEventListener('click', resizeGridHandler);
+rndColorBtn.addEventListener('click', toggleClrHandler);
 
-function resizeGrid() {
+function updateCellListeners() {
+    const colNodes = document.querySelectorAll('.grid-column');
+
+    colNodes.forEach(el => {
+        el.addEventListener('mouseover', colorCellHandler)
+        el.addEventListener('mousedown', colorCellHandler)
+        el.addEventListener('mouseup', colorCellHandler)
+    })
+}
+
+function resetGrid() {
+    gridSizeSlider.value = 64;
+    gridSizeField.textContent = `${gridSizeSlider.value}x${gridSizeSlider.value}`;
+    resizeGridHandler();
+}
+
+function resizeGridHandler(event) {
     grid.innerHTML = '';
 
     let gridSize = gridSizeSlider.value;
@@ -30,20 +46,27 @@ function resizeGrid() {
             newRow.appendChild(newColumn);
         }
     }
-    updateListeners();
+    updateCellListeners();
 }
 
-function resetGrid() {
-    gridSizeSlider.value = 64;
-    gridSizeField.textContent = `${gridSizeSlider.value}x${gridSizeSlider.value}`;
-    resizeGrid();
+function colorCellHandler(event) {
+    if (event.type === 'mousedown') mouseToggle = true;
+    if (mouseToggle) {
+        if (!isColorRandom) {
+            this.style.backgroundColor = 'black';
+        } else {
+            this.style.backgroundColor =
+                `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+        }
+    }
+    if (event.type === 'mouseup') mouseToggle = false;
 }
 
-function updateSizeField(e) {
+function updSizeFieldHandler(event) {
     gridSizeField.textContent = `${this.value}x${this.value}`;
 }
 
-function toggleColoring(e) {
+function toggleClrHandler(event) {
     isColorRandom = !isColorRandom;
 
     if (isColorRandom) {
@@ -56,28 +79,5 @@ function toggleColoring(e) {
     }
 }
 
-function updateListeners() {
-    const colNodes = document.querySelectorAll('.grid-column');
-
-    colNodes.forEach(el => {
-        el.addEventListener('mouseover', colorCell)
-        el.addEventListener('mousedown', colorCell)
-        el.addEventListener('mouseup', colorCell)
-    })
-}
-
-function colorCell(e) {
-    if (e.type === 'mousedown') mouseToggle = true;
-    if (mouseToggle) {
-        if (!isColorRandom) {
-            this.style.backgroundColor = 'black';
-        } else {
-            this.style.backgroundColor =
-                `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
-        }
-    }
-    if (e.type === 'mouseup') mouseToggle = false;
-}
-
-resizeGrid(64, 64);
-updateListeners();
+resizeGridHandler(64, 64);
+updateCellListeners();
