b0ba0c1fa4cc92c685a8b98e5750f33827b6f991
Siriuszx <114768817+Siriuszx@users.noreply.github.com>
2023-07-28 12:31:43 +0200

    Add cell functionality
    Reassign listeners to each cell when needed, add colorCell functionality

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const grid = document.querySelector('.grid');
 
-let width = 64;
-let height = 64;
+function initGrid(width, height) {
+    for (let i = 0; i < height; i++) {
+        const newRow = document.createElement('div');
+        newRow.classList.add('grid-row');
+        grid.appendChild(newRow);
 
-console.log(grid);
-
-for (let i = 0; i < height; i++) {
-    let newRow = document.createElement('div');
-    newRow.classList.add('grid-row');
-    grid.appendChild(newRow);
-    
-    for (let y = 0; y < width; y++) {
-        let newColumn = document.createElement('div');
-        newColumn.classList.add('grid-column');
-        newRow.appendChild(newColumn);    
+        for (let y = 0; y < width; y++) {
+            const newColumn = document.createElement('div');
+            newColumn.classList.add('grid-column');
+            newRow.appendChild(newColumn);
+        }
     }
 }
+
+function updateListeners() {
+    const colNodes = document.querySelectorAll('.grid-column');
+
+    colNodes.forEach(el => {
+        el.addEventListener('mouseover', colorCell)
+    })
+}
+
+function colorCell(e) {
+    this.style.backgroundColor = 'black';
+}
+
+initGrid(64, 64);
+updateListeners();
