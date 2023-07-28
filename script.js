f0706696f19e70e5a4efa30ba4c2350234c12683
Siriuszx <114768817+Siriuszx@users.noreply.github.com>
2023-07-28 14:48:03 +0200

    Add smooth slider, reset button

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ const grid = document.querySelector('.grid');
 
 const gridSizeField = document.querySelector('#current-grid-size')
 const gridSizeSlider = document.querySelector('#grid-dimensions-slider');
-
-gridSizeSlider.addEventListener('mouseover', updateSizeValue)
-gridSizeSlider.addEventListener('mousedown', updateSizeValue)
-gridSizeSlider.addEventListener('mouseup', updateSizeValue)
-
+gridSizeSlider.addEventListener('input', updateSizeField);
 
 let mouseToggle = false;
 
-const submitSize = document.querySelector('.select-value-submit');
-submitSize.addEventListener('click', resizeGrid);
+const clearBtn = document.querySelector('.select-value-clear');
+const submitBtn = document.querySelector('.select-value-submit');
+clearBtn.addEventListener('click', resetGrid)
+submitBtn.addEventListener('click', resizeGrid);
 
 function resizeGrid() {
     grid.innerHTML = '';
@@ -32,10 +30,14 @@ function resizeGrid() {
     updateListeners();
 }
 
-function updateSizeValue(e) {
-    if (e.type === 'mousedown') mouseToggle = true;
-    if (e.type === 'mouseover' && mouseToggle) gridSizeField.textContent = this.value;
-    if (e.type === 'mouseup') mouseToggle = false;
+function resetGrid(){
+    gridSizeSlider.value = 64;
+    gridSizeField.textContent = `${gridSizeSlider.value}x${gridSizeSlider.value}`;
+    resizeGrid();
+}
+
+function updateSizeField(e) {
+    gridSizeField.textContent = `${this.value}x${this.value}`;
 }
 
 function updateListeners() {
