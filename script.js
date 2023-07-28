512691140523c7ea14058b41243072787274fcbc
Siriuszx <114768817+Siriuszx@users.noreply.github.com>
2023-07-28 15:06:08 +0200

    Add random color mode, rndColor button

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const gridSizeSlider = document.querySelector('#grid-dimensions-slider');
 gridSizeSlider.addEventListener('input', updateSizeField);
 
 let mouseToggle = false;
+let isColorRandom = false;
 
 const clearBtn = document.querySelector('.select-value-clear');
 const submitBtn = document.querySelector('.select-value-submit');
+const rndColorBtn = document.querySelector('.select-color');
 clearBtn.addEventListener('click', resetGrid)
 submitBtn.addEventListener('click', resizeGrid);
+rndColorBtn.addEventListener('click', toggleColoring);
 
 function resizeGrid() {
     grid.innerHTML = '';
@@ -30,7 +33,7 @@ function resizeGrid() {
     updateListeners();
 }
 
-function resetGrid(){
+function resetGrid() {
     gridSizeSlider.value = 64;
     gridSizeField.textContent = `${gridSizeSlider.value}x${gridSizeSlider.value}`;
     resizeGrid();
@@ -38,6 +41,19 @@ function resetGrid(){
 
 function updateSizeField(e) {
     gridSizeField.textContent = `${this.value}x${this.value}`;
+}
+
+function toggleColoring(e) {
+    isColorRandom = !isColorRandom;
+
+    if (isColorRandom) {
+        rndColorBtn.style.backgroundColor = 'purple';
+        rndColorBtn.style.color = 'white';
+    }
+    else {
+        rndColorBtn.style.backgroundColor = '';
+        rndColorBtn.style.color = '';
+    }
 }
 
 function updateListeners() {
@@ -52,7 +68,14 @@ function updateListeners() {
 
 function colorCell(e) {
     if (e.type === 'mousedown') mouseToggle = true;
-    if (e.type === 'mouseover' && mouseToggle) this.style.backgroundColor = 'black';
+    if (mouseToggle) {
+        if (!isColorRandom) {
+            this.style.backgroundColor = 'black';
+        } else {
+            this.style.backgroundColor =
+                `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+        }
+    }
     if (e.type === 'mouseup') mouseToggle = false;
 }
 
